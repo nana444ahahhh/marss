d3e8a617867c6cf1621cea764e4ab930a5a40f18
primelost <80356414+nana444ahahhh@users.noreply.github.com>
2023-02-04 10:07:13 +0300

    Add files via upload

--- mars.py
+++ mars.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import url_for
 from flask import request
+
 app = Flask(__name__)
 
 i = 0
@@ -30,7 +31,7 @@ def promote():
                         <h1>Мы сделаем обитаемыми безжизненные пока планеты.</h1>
                         <h1>И начнем с Марса!</h1>
                         <h1>Присоединяйся!</h1>
-                        
+
                       </body>
                     </html>"""
 
@@ -46,7 +47,7 @@ def image():
                       <body>
                         <h1>Жди нас, Марс!</h1>
                         <
-                        
+
                       </body>
                     </html>
     <img src="{url_for('static', filename='c2c17f31b88d4ea800bfe289258d40b8.jpg')}" 
@@ -55,7 +56,9 @@ def image():
 
 @app.route('/promotion_image')
 def promimage():
-    return f'''<!doctype html>
+    return f'''
+    <!doctype html> <img src="{url_for('static', filename='c2c17f31b88d4ea800bfe289258d40b8.jpg')}" 
+           alt="здесь должна была быть картинка, но не нашлась">
                     <html lang="en">
                       <head>
                         <meta charset="utf-8">
@@ -87,9 +90,9 @@ def promimage():
                         </div>
                       </body>
                     </html>
-                    
-                    <img src="{url_for('static', filename='c2c17f31b88d4ea800bfe289258d40b8.jpg')}" 
-           alt="здесь должна была быть картинка, но не нашлась">'''
+
+                   '''
+
 
 @app.route('/form_sample', methods=['POST', 'GET'])
 def form_sample():
@@ -146,36 +149,36 @@ def form_sample():
                                     </div>
                                      <label for="form-group form-check">Укажите работу</label>
                                     <div class="form-group form-check">
-                                    
+
                                         <input type="checkbox" class="form-check-input" id="acceptRules" name="accept">
-                                        
+
                                         <label class="form-check-label" for="acceptRules">врач</label>
-                                        
-                                        
-        
+
+
+
                                     </div>
                                      <div class="form-group form-check">
-                                    
+
                                         <input type="checkbox" class="form-check-input" id="acceptRules" name="accept">
-                                        
+
                                         <label class="form-check-label" for="acceptRules">инженер-исследователь</label>
-                                        
-                                        
-        
+
+
+
                                     </div>
                                       <div class="form-group">
                                         <label for="photo">Приложите фотографию</label>
                                         <input type="file" class="form-control-file" id="photo" name="file">
                                     </div>
-                                    
+
                                      <div class="туц">
-                                    
+
                                         <input type="checkbox" class="ready?" id="acceptRules" name="accept">
-                                        
+
                                         <label class="form-check-label" for="acceptRules">готовы ли вы остаться на марсе?</label>
-                                        
-                                        
-        
+
+
+
                                     </div>
                                     <button type="submit" class="btn btn-primary">Записаться</button>
                                 </form>
@@ -191,5 +194,46 @@ def form_sample():
         print(request.form['accept'])
         print(request.form['sex'])
         return "Форма отправлена"
+
+
+@app.route('/choice/<planetname>')
+def choicce(planetname):
+    if request.method == 'GET':
+        return f'''
+                            <html lang="en">
+                              <head>
+                                <meta charset="utf-8">
+                                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                                <link rel="stylesheet" 
+                                href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                                integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                                crossorigin="anonymous">
+                                <title>Реклама {planetname}!</title>
+                              </head>
+                              <body>
+                               <meta charset="utf-8">
+                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                            <h1>идите на {planetname}</h1>
+                                <div class="alert alert-primary" role="alert">
+                                 Переезжайте на {planetname}
+                                </div>
+                                <div class="alert alert-success" role="alert">
+                                  там лучше чем на земле
+                                </div>
+                                <div class="alert alert-danger" role="alert">
+                                  тут весело жить
+                                </div>
+                                <div class="alert alert-info" role="alert">
+                                  легче зарабатывать
+                                </div>
+                                <div class="alert alert-dark" role="alert">
+                                  ты будешь Богаче!
+                                </div>
+                              </body>
+                            </html>
+
+                           '''
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
